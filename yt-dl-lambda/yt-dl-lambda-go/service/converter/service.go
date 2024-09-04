2c0eadd176dbe594930ed410603ec085af582c42
Gage Cottom <gagecottom@hotmail.com>
2024-09-04 10:43:13 -0500

    update converter

--- yt-dl-lambda/yt-dl-lambda-go/service/converter/service.go
+++ yt-dl-lambda/yt-dl-lambda-go/service/converter/service.go
@@ -13,36 +13,26 @@ import (
 	"go.uber.org/zap"
 )
 
-func Convert(input []byte, id string) error {
-	var args = []string{"-i", "pipe:0", "-acodec:a", "libmp3lame", "-b:a", "256k", "-f", "mp3", "-"}
+func Convert(input *os.File, id string) error {
+	var args = []string{"-i", fmt.Sprintf("/tmp/%s", id), "-c:a", "libmp3lame", "-b:a", "256k", "-f", "mp3", fmt.Sprintf("/tmp/%s.mp3", id)}
 	cmd := exec.Command(path.Join(os.Getenv("LAMBDA_TASK_ROOT"), "ffmpeg"), args...)
-	resultBuffer := bytes.NewBuffer(make([]byte, 0))
-	errBuffer := bytes.NewBuffer(make([]byte, 0))
-	cmd.Stderr = errBuffer
-	cmd.Stdout = resultBuffer
-	stdin, err := cmd.StdinPipe()
+
+	if err := cmd.Start(); err != nil {
+		zaplog.Error("Failed to start ffmpeg", zap.Error(err))
+		return err
+	}
+	if err := cmd.Wait(); err != nil {
+		zaplog.Error("Failed to wait for ffmpeg", zap.Error(err))
+		return err
+	}
+	defer os.Remove(fmt.Sprintf("/tmp/%s.mp3", id))
+	data, err := os.ReadFile(fmt.Sprintf("/tmp/%s.mp3", id))
 	if err != nil {
-		zaplog.Error("Failed to create stdin pipe", zap.Error(err))
+		zaplog.Error("Failed to read file", zap.Error(err))
 		return err
 	}
-	if err = cmd.Start(); err != nil {
-		zaplog.Error("Failed to start ffmpeg", zap.Error(err), zap.String("stderr", errBuffer.String()))
-		return err
-	}
-	if _, err = stdin.Write(input); err != nil {
-		zaplog.Error("Failed to write to stdin", zap.Error(err))
-		return err
-	}
-	if err = stdin.Close(); err != nil {
-		zaplog.Error("Failed to close stdin", zap.Error(err))
-		return err
-	}
-	if err = cmd.Wait(); err != nil {
-		zaplog.Error("Failed to wait for ffmpeg", zap.Error(err), zap.String("stderr", errBuffer.String()))
-		return err
-	}
-	if _, err = retry.Retry(retry.NewAlgSimpleDefault(), 3, s3.UploadToS3,
-		resultBuffer, fmt.Sprintf("%s.mp3", id), s3.YTDLS3Bucket); err != nil {
+	if _, err := retry.Retry(retry.NewAlgSimpleDefault(), 3, s3.UploadToS3,
+		bytes.NewReader(data), fmt.Sprintf("%s.mp3", id), s3.YTDLS3Bucket); err != nil {
 		zaplog.Error("Failed to upload to s3", zap.Error(err))
 		return err
 	}
